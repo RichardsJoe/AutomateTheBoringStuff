8ae1825f1099bc129845a28bbea69e2a0ac04fb7
= <=>
2025-01-26 14:15:00 -0500

    4th

--- 0402_Coin_Flip_Streaks.py
+++ 0402_Coin_Flip_Streaks.py
@@ -1,3 +1,6 @@
+#Determine the odds a 6 consecutive H or T flips.
+#Based on how many flips are done and how many times this is repeated
+
 import random
 
 def flip_coin(n_flips):
@@ -12,8 +15,30 @@ def flip_coin(n_flips):
         results.append(result)
     return results
 
-def count_streak(streak_length, flip_results):
-    #number of n_count streaks for H or T inside the results.
-    pass
+def has_streak(flip_results):
+    has_streak = False
+    #convert the list into a string
+    result_string = ''
+    for result in flip_results:
+        result_string += result
+    
+    if ('HHHHHH') in result_string:
+        has_streak = True
+    if ('TTTTTT') in result_string:
+        has_streak = True
+    
+    return has_streak
+        
+        
+experimentNumber = 10000
+n_streaks = 0
+for count in range(experimentNumber):
+    if has_streak(flip_coin(100)) == True:
+        
+        n_streaks += 1
+    else:
+        continue
+print(n_streaks)
 
-(flip_coin(100))
+probability_of_streak = (n_streaks / experimentNumber) * 100  
+print(f"Chance of streak: {probability_of_streak}%")
